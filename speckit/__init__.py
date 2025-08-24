5ea5efc7e7139c37a4a10bc6fdc613f288b542fd
Miguel Dovale <mikedovale@pm.me>
2025-08-24 11:09:32 -0700

    Move _config.py contents to __init__.py

--- speckit/__init__.py
+++ speckit/__init__.py
@@ -1,3 +1,14 @@
-from ._config import *
-from .analysis import compute_spectrum, compute_single_bin, lpsd
-from .core import SpectrumAnalyzer, SpectrumResult
+import os
+os.environ.setdefault("NUMBA_THREADING_LAYER", "workqueue")
+os.environ.setdefault("NUMBA_NUM_THREADS", str(max(1, (os.cpu_count() or 1))))
+os.environ.setdefault("OMP_NUM_THREADS", "1")
+os.environ.setdefault("MKL_NUM_THREADS", "1")
+os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
+
+from .analysis import (
+    compute_spectrum, 
+    compute_single_bin, 
+    lpsd, 
+    SpectrumAnalyzer, 
+    SpectrumResult
+)
